c59f4b8cba829e8758b859d03250fc29fc09d0ac
Tamer Aydin <tamer.aydin@gmail.com>
2015-02-19 23:59:20 +0100

    updated dist files

--- dist/carouselar.js
+++ dist/carouselar.js
@@ -8,31 +8,94 @@
 
   angular
     .module('carouselar.directives', [
-      'carouselar.provider'
-    ])
-    .directive('carouselar', ['carouselar',
-      function(carouselar) {
+      'carouselar.controllers', 'carouselar.constants'])
+    .directive('carouselar', ['$window', 'CarouselarConstants',
+      function($window, CarouselarConstants) {
         return {
           replace: true,
           restrict: 'E',
           scope: {
-            maxImage: '=',
             images: '='
           },
-          template: '<div></div>',
-          link: function() {
-            console.log(1, carouselar);
+          controller: 'CarouselarController',
+          template:
+            '<div class="carouselar">' +
+              '<div class="carouselar__container">' +
+                '<div class="carouselar__inner-container" ' +
+                'ng-style="{\'transform\': containerPosition}">' +
+                  '<div class="carouselar__image-container" ' +
+                    'ng-style="{\'width\': singleImageWidth}" ' +
+                    'ng-repeat="image in images" ' +
+                    'is-visible="isImageVisible($index)" ' +
+                    'img-url="{{image}}" ' +
+                    'carouselar-image>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+              '<div class="carouselar__navigation">' +
+                '<span class="carouselar__navigation__button ' +
+                'carouselar__navigation__button--prev" ng-click="prev()">PREV</span>' +
+                '<div class="carouselar__navigation__index">' +
+                  '<span class="carouselar__navigation__button ' +
+                    'carouselar__navigation__button--index" ' +
+                    'ng-repeat="i in createArray(sectionCount) track by $index" ' +
+                    'ng-class="{\'carouselar__navigation__button--active\':$index === activeSection}"' +
+                    'ng-click="moveToSection($index)">' +
+                      '{{$index + 1}}' +
+                  '</span>' +
+                '</div>' +
+                '<span class="carouselar__navigation__button ' +
+                'carouselar__navigation__button--next" ng-click="next()">NEXT</span>' +
+              '</div>' +
+            '</div>',
+          link: function(scope, element, attrs) {
+            scope.maxImageCount = Math.min(
+              parseInt(attrs.maxImageCount), CarouselarConstants.MAX_DISPLAYED_IMAGE) || 1;
+
+            var win = angular.element($window);
+            win.bind('resize', scope.onResize);
+            scope.onResize();
+
+            scope.$on('destroy', function() {
+              win.unbind(scope.onResize);
+            });
           }
         };
       }
     ])
-    .directive('carouselarImage', ['carouselar',
-      function(carouselar) {
+    .directive('carouselarImage', ['$compile',
+      function($compile) {
         return {
-          replace: true,
-          restrict: 'E',
-          link: function() {
-            console.log(2, carouselar);
+          restrict: 'A',
+          controller: 'CarouselarImageController',
+          scope: {
+            isVisible: '=',
+            imgUrl: '@'
+          },
+          template: '<div class="carouselar__loader" ng-if="isLoading"></div>',
+          link: function(scope, element) {
+            scope.compileImage = function() {
+              element.append(
+                '<img ' +
+                  'class="carouselar__image" ' +
+                  'carouselar-loader ' +
+                  'on-load="onLoad()" ' +
+                  'ng-src="{{imgUrl}}" />');
+              $compile(element.contents())(scope);
+            };
+          }
+        };
+      }
+    ])
+    .directive('carouselarLoader', [
+      function() {
+        return {
+          restrict: 'A',
+          scope: {
+            onLoad: '&'
+          },
+          link: function(scope, element) {
+            element.bind('load', scope.onLoad);
           }
         };
       }
@@ -45,7 +108,9 @@
 
   angular
     .module('carouselar', [
-      'carouselar.directives'
+      'carouselar.controllers',
+      'carouselar.directives',
+      'carouselar.constants'
     ]);
 
 })(window, window.angular);
