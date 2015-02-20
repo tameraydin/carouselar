6b6c37ffffa12e71d8b25ef18c2ff10f5abfe842
Tamer Aydin <tamer.aydin@gmail.com>
2015-02-21 00:45:04 +0100

    fixed broken e2e tests on travis-ci

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -1,4 +1,15 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['specs/e2e/carouselar.js']
+  specs: ['specs/e2e/carouselar.js'],
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': [
+        'no-sandbox',
+        'no-default-browser-check',
+        'no-first-run',
+        'disable-default-apps'
+      ]
+    }
+  }
 };
